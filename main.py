8003fef769e49b30319f71c113a3b326102968cf
Matej Dujava <mdujava@redhat.com>
2015-12-09 10:44:27 +0100

    Refractoring.

--- main.py
+++ main.py
@@ -3,44 +3,52 @@ import codecs
 import sys
 
 def main(fileName = 'test'):
-	inputFile = codecs.open(fileName + '.txt', 'r', 'utf-8')
-	r = open(fileName + '-basic.txt', 'w')
-	s = open(fileName + '-sentences.txt', 'w')
-	w = open(fileName + '-word.txt', 'w')
-	c = open(fileName + '-char.txt', 'w')
+    inputFile = codecs.open(fileName + '.txt', 'r', 'utf-8')
+    resultFile = open(fileName + '-basic.txt', 'w')
+    sentenceFile = open(fileName + '-sentences.txt', 'w')
+    wordFile = open(fileName + '-word.txt', 'w')
+    charFile = open(fileName + '-char.txt', 'w')
+    charFile2 = open(fileName + '-char-2.txt', 'w')
 
-	# text from file. 
-	inputString = inputFile.read()
+    # text from file. 
+    inputString = inputFile.read()
 
-	# list of lines
-	strings = inputString.splitlines(True)
+    # list of lines
+    strings = inputString.splitlines(True)
 
-	# string witout non Alfanum characters (with . ! ?)
-	escapedString, lineCount = escape(strings)
+    # string witout non Alfanum characters (with . ! ?)
+    escapedString, lineCount = escape(strings)
 
-	# word and sentence statistics
-	words_final, wordCount, sentenceCount, sentences_final = wordStats(escapedString)
+    # word and sentence statistics
+    words_final, wordCount, sentenceCount, sentences_final = wordStats(escapedString)
 
-	# char statistics
-	char_final, charCount = charStats(escapedString)
+    # char statistics orderd by count
+    char_final, charCount = charStats(escapedString, 1)
 
-	# writting basic stats to file
-	stats_basic(r, lineCount, wordCount, charCount, sentenceCount)
+    # char statistics orderd by Alphabet
+    char_final_2, charCount_2 = charStats(escapedString, 2)
 
-	# writting word stats to file
-	dunmpStats(w, words_final)
-	
-	# writting sentence stats to file
-	dunmpStats(s, sentences_final)
+    # writting basic stats to file
+    stats_basic(resultFile, lineCount, wordCount, charCount, sentenceCount)
 
-	# writting character stats to file
-	dunmpStats(c, char_final)
+    # writting word stats to file
+    dunmpStats(wordFile, words_final)
+    
+    # writting sentence stats to file
+    dunmpStats(sentenceFile, sentences_final)
 
-	inputFile.close()
-	r.close()
-	w.close()
-	s.close()
-	c.close()
+    # writting character stats to file
+    dunmpStats(charFile, char_final)
+
+    # writting character stats to file
+    dunmpStats(charFile2, char_final_2)
+
+    inputFile.close()
+    resultFile.close()
+    sentenceFile.close()
+    wordFile.close()
+    charFile.close()
+    charFile2.close()
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
--- stats.py
+++ stats.py
@@ -2,107 +2,98 @@ import operator
 
 
 def escape(strings):
-	string = ''
-	lineCount = 0
-	
-	for x in strings:
-		newLine = x.replace('\n', ' ')
-		newLine.strip()
-		if len(newLine):
-			lineCount += 1
-		string += newLine
+    string = ''
+    lineCount = 0
+    
+    for x in strings:
+        newLine = x.replace('\n', ' ')
+        newLine = x.replace('\t', ' ')
+        newLine.strip()
+        if len(newLine):
+            lineCount += 1
+        string += newLine
 
-	string = string.replace(':', '')
-	string = string.replace(';', '')
-	string = string.replace(',', '')
-	string = string.replace('/', '')
-	string = string.replace('\\', '')
-	string = string.replace('"', '')
-	string = string.replace('\'', '')
-	string = string.replace('_', '')
-	string = string.replace(']', ' ')
-	string = string.replace('[', ' ')
-	string = string.replace('(', ' ')
-	string = string.replace('(', ' ')
-	string = string.replace('{', ' ')
-	string = string.replace('}', ' ')
-	string = string.replace('|', ' ')
-	string = string.replace(' - ', ' ')
+    nonAlphaNumeric = ':;,/\\"\'_[]{}()|#*+-=<>~`°'
 
-	return [string, lineCount]
+    for char in nonAlphaNmeric:
+        string = string.replace(char, ' ')
+
+    return [string, lineCount]
 
 def wordStats(escapedString):
-	words = {}
-	sentences = {}
-	wordCount = 0
-	wordCountInSentence = 0
-	sentenceCount = 0
+    words = {}
+    sentences = {}
+    wordCount = 0
+    wordCountInSentence = 0
+    sentenceCount = 0
 
-	for word in escapedString.split(' '):
-		wordCountInSentence +=1
-		if "." in word or "!" in word or "?" in word:
-			if wordCountInSentence != 0:
-				if wordCountInSentence in sentences.keys():
-					sentences[wordCountInSentence] += 1
-				else:
-					sentences[wordCountInSentence] = 1
-				sentenceCount += 1
-				wordCountInSentence = 0
-			word = word.replace(".", "")
-			word = word.replace("!", "")
-			word = word.replace("?", "")
-		wordCount += 1 
-		if word != '':
-			if word.lower() in words.keys():
-				words[word.lower()] += 1
-			else:
-				words[word.lower()] = 1
+    for word in escapedString.split(' '):
+        wordCountInSentence +=1
+        if "." in word or "!" in word or "?" in word:
+            if wordCountInSentence != 0:
+                if wordCountInSentence in sentences.keys():
+                    sentences[wordCountInSentence] += 1
+                else:
+                    sentences[wordCountInSentence] = 1
+                sentenceCount += 1
+                wordCountInSentence = 0
+            word = word.replace(".", "")
+            word = word.replace("!", "")
+            word = word.replace("?", "")
+        wordCount += 1 
+        if word != '':
+            if word.lower() in words.keys():
+                words[word.lower()] += 1
+            else:
+                words[word.lower()] = 1
 
-	sentences_final = sorted(sentences.items(), key=operator.itemgetter(0))
+    sentences_final = sorted(sentences.items(), key=operator.itemgetter(0))
 
-	words_final = sorted(words.items(), key=operator.itemgetter(1))
-	words_final = words_final[::-1]
+    words_final = sorted(words.items(), key=operator.itemgetter(1))
+    words_final = words_final[::-1]
 
-	return [words_final, wordCount, sentenceCount, sentences_final] 
+    return [words_final, wordCount, sentenceCount, sentences_final] 
 
-def charStats(escapedString):
-	chars = {}
-	charCount = 0
+def charStats(escapedString, order = 1):
+    chars = {}
+    charCount = 0
 
-	for word in escapedString.split(' '):
-		for char in word:
-			if char != '':
-				charCount += 1
-				if char.lower() in chars.keys():
-					chars[char.lower()] += 1
-				else:
-					chars[char.lower()] = 1
+    for word in escapedString.split(' '):
+        for char in word:
+            if char != '':
+                charCount += 1
+                if char.lower() in chars.keys():
+                    chars[char.lower()] += 1
+                else:
+                    chars[char.lower()] = 1
 
-	# order by count
-	#char_final = sorted(chars.items(), key=operator.itemgetter(1))
-	#char_final = char_final[::-1]
+    # order by count
+    if order == 1:
+        char_final = sorted(chars.items(), key=operator.itemgetter(1))
+        char_final = char_final[::-1]
 
-	# order by alphabet
-	char_final = sorted(chars.items(), key=operator.itemgetter(0))
+    # order by alphabet
+    if order == 2:
+        char_final = sorted(chars.items(), key=operator.itemgetter(0))
 
 
-	return [char_final, charCount]
+    return [char_final, charCount]
 
 
 def stats_basic(stat_file, lineCount, wordCount, charCount, sentenceCount):
 
-	stat_file.write("Line count: " + str(lineCount) + "\n")
-	stat_file.write("Word count: " + str(wordCount) + "\n")
-	stat_file.write("Char count: " + str(charCount) + "\n")
-	stat_file.write("Sentence count: " + str(sentenceCount) + "\n")
-	stat_file.write("\n")
-	stat_file.write("Words per line(avg): " + str(round(wordCount/lineCount, 2)) + "\n")
-	stat_file.write("Words per sentence(avg): " + str(round(wordCount/sentenceCount, 2)) + "\n")
-	stat_file.write("\n")
-	stat_file.write("Char per line(avg): " + str(round(charCount/lineCount, 2)) + "\n")
-	stat_file.write("Char per sentence(avg): " + str(round(charCount/sentenceCount, 2)) + "\n")
-	stat_file.write("Char per word(avg): " + str(round(charCount/wordCount, 2)) + "\n")
+    stat_file.write("Line count: " + str(lineCount) + "\n")
+    stat_file.write("Word count: " + str(wordCount) + "\n")
+    stat_file.write("Char count: " + str(charCount) + "\n")
+    stat_file.write("Sentence count: " + str(sentenceCount) + "\n")
+    stat_file.write("\n")
+    stat_file.write("Words per line(avg): " + str(round(wordCount/lineCount, 2)) + "\n")
+    stat_file.write("Words per sentence(avg): " + str(round(wordCount/sentenceCount, 2)) + "\n")
+    stat_file.write("\n")
+    stat_file.write("Char per line(avg): " + str(round(charCount/lineCount, 2)) + "\n")
+    stat_file.write("Char per sentence(avg): " + str(round(charCount/sentenceCount, 2)) + "\n")
+    stat_file.write("Char per word(avg): " + str(round(charCount/wordCount, 2)) + "\n")
 
 def dunmpStats(stat_file, data):
-	for item in data:
-		stat_file.write(str(item[0]) + "," + str(item[1])  + "\n")
+    for item in data:
+        stat_file.write(str(item[0]) + "," + str(item[1])  + "\n")
