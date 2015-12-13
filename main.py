0969c0afcc1faafb44f3f62ea4775af7ccae17fe
matejd11 <mdujava@gmail.com>
2015-12-13 19:40:57 +0100

    Start to code comparing.

--- main.py
+++ main.py
@@ -1,57 +1,25 @@
 from stats import *
-import codecs
 import sys
 
-def main(fileName = 'test'):
-    inputFile = codecs.open(fileName + '.txt', 'r', 'utf-8')
-    resultFile = open(fileName + '-basic.txt', 'w')
-    sentenceFile = open(fileName + '-sentences.txt', 'w')
-    wordFile = open(fileName + '-word.txt', 'w')
-    charFile = open(fileName + '-char.txt', 'w')
-    charFile2 = open(fileName + '-char-2.txt', 'w')
+def main(fileName):
+    secondRes = None 
+    secondFileName = None
+    if isinstance(fileName, list):
+        secondFileName = fileName.pop()
+        secondRes = completeStats(secondFileName)  
+        fileName = fileName[0]
+    res = completeStats(fileName)
 
-    # text from file. 
-    inputString = inputFile.read()
-
-    # list of lines
-    strings = inputString.splitlines(True)
-
-    # string witout non Alfanum characters (with . ! ?)
-    escapedString, lineCount = escape(strings)
-
-    # word and sentence statistics
-    words_final, wordCount, sentenceCount, sentences_final = wordStats(escapedString)
-
-    # char statistics orderd by count
-    char_final, charCount = charStats(escapedString, 1)
-
-    # char statistics orderd by Alphabet
-    char_final_2, charCount_2 = charStats(escapedString, 2)
-
-    # writting basic stats to file
-    stats_basic(resultFile, lineCount, wordCount, charCount, sentenceCount)
-
-    # writting word stats to file
-    dunmpStats(wordFile, words_final)
+    if secondRes != None:
+        print("comperring res")
+        for x in res:
+            print(compareStats(x, res[x], secondRes[x]))
     
-    # writting sentence stats to file
-    dunmpStats(sentenceFile, sentences_final)
-
-    # writting character stats to file
-    dunmpStats(charFile, char_final)
-
-    # writting character stats to file
-    dunmpStats(charFile2, char_final_2)
-
-    inputFile.close()
-    resultFile.close()
-    sentenceFile.close()
-    wordFile.close()
-    charFile.close()
-    charFile2.close()
 
 if __name__ == '__main__':
-    if len(sys.argv) == 2:
+    if len(sys.argv) == 3:
+        main([sys.argv[1],sys.argv[2]])
+    elif len(sys.argv) == 2:
         main(sys.argv[1])
     else:
-        main()
+        main("input")
--- stats.py
+++ stats.py
@@ -1,5 +1,100 @@
 import operator
+import codecs
 
+
+def compareStats(name, resA, resB):    
+    res = "difference in " + name + ":"
+
+    if name == "basic":
+        for i in range(len(resA)):
+            diff = resB[i] - resA[i] 
+            if i == 0:
+                res += "\n\tin line count: "
+            elif i == 1:
+                res += "\n\tin word count: "
+            elif i == 2:
+                res += "\n\tin char count: "
+            elif i == 3:
+                res += "\n\tin centences count:"
+            res += str(diff)            
+
+    if name == "word":
+        A = resA[0] 
+        B = resB[0] 
+        for i in range(len(A)):
+            a1, a2 = A[i]
+            b1, b2 = B[i]
+            print(a2, b2)
+            diff = b2 -a2 
+            if i == 0:
+                res += "\n\tin line count: "
+            elif i == 1:
+                res += "\n\tin word count: "
+            elif i == 2:
+                res += "\n\tin char count: "
+            elif i == 3:
+                res += "\n\tin centences count:"
+            res += str(diff)            
+    
+    res += "\n" 
+    return res
+
+def completeStats(fileName):
+    inputFile = codecs.open(fileName + '.txt', 'r', 'utf-8')
+    resultFile = open(fileName + '-basic.txt', 'w')
+    sentenceFile = open(fileName + '-sentences.txt', 'w')
+    wordFile = open(fileName + '-word.txt', 'w')
+    charFile = open(fileName + '-char.txt', 'w')
+    charFile2 = open(fileName + '-char-2.txt', 'w')
+
+    # text from file. 
+    inputString = inputFile.read()
+
+    # list of lines
+    strings = inputString.splitlines(True)
+
+    # string witout non Alfanum characters (with . ! ?)
+    escapedString, lineCount = escape(strings)
+
+    # word and sentence statistics
+    words_final, wordCount, sentenceCount, sentences_final = wordStats(escapedString)
+
+    # char statistics orderd by count
+    char_final, charCount = charStats(escapedString, 1)
+
+    # char statistics orderd by Alphabet
+    char_final_2, charCount_2 = charStats(escapedString, 2)
+
+    # writting basic stats to file
+    stats_basic(resultFile, lineCount, wordCount, charCount, sentenceCount)
+
+    # writting word stats to file
+    dumpPartialStats(wordFile, words_final)
+    
+    # writting sentence stats to file
+    dumpPartialStats(sentenceFile, sentences_final)
+
+    # writting character stats to file
+    dumpPartialStats(charFile, char_final)
+
+    # writting character stats to file
+    dumpPartialStats(charFile2, char_final_2)
+
+    inputFile.close()
+    resultFile.close()
+    sentenceFile.close()
+    wordFile.close()
+    charFile.close()
+    charFile2.close()
+
+    res = {
+            "basic" : [lineCount, wordCount, charCount, sentenceCount],
+            "word" : [words_final],
+            "sentance" : [sentences_final],
+            "char" : [char_final, char_final_2]
+          }
+
+    return res
 
 def escape(strings):
     string = ''
@@ -15,7 +110,7 @@ def escape(strings):
 
     nonAlphaNumeric = ':;,/\\"\'_[]{}()|#*+-=<>~`°'
 
-    for char in nonAlphaNmeric:
+    for char in nonAlphaNumeric:
         string = string.replace(char, ' ')
 
     return [string, lineCount]
@@ -94,6 +189,6 @@ def stats_basic(stat_file, lineCount, wordCount, charCount, sentenceCount):
     stat_file.write("Char per sentence(avg): " + str(round(charCount/sentenceCount, 2)) + "\n")
     stat_file.write("Char per word(avg): " + str(round(charCount/wordCount, 2)) + "\n")
 
-def dunmpStats(stat_file, data):
+def dumpPartialStats(stat_file, data):
     for item in data:
         stat_file.write(str(item[0]) + "," + str(item[1])  + "\n")
